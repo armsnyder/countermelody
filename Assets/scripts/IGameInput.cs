ae7a56fa0b5e178b881f47f5501bd0dc4f2ed201
cegrief <cegrief@gmail.com>
2016-02-03 21:01:26 -0600

    Input interface, and started keyboard input

---
+++ Assets/scripts/IGameInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public interface IGameInput {
+
+    event EventHandler green;
+    event EventHandler red;
+    event EventHandler yellow;
+    event EventHandler blue;
+    event EventHandler orange;
+
+    event EventHandler green_strum;
+    event EventHandler red_strum;
+    event EventHandler yellow_strum;
+    event EventHandler blue_strum;
+    event EventHandler orange_strum;
+
+    event EventHandler whammy_down;
+    event EventHandler whammy_up;
+
+}
