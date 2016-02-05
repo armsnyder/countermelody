f187d720750e387f24c6c4ba83460963dded8b6b
Adam Snyder <armsnyder@gmail.com>
2016-02-05 16:11:06 -0600

    Adds pseudo Song class that sends beat events

---
+++ Assets/scripts/Song.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Frictionless;
+
+public class EnterBeatWindowMessage {}
+
+public class ExitBeatWindowMessage {}
+
+public class Song : MonoBehaviour {
+
+	public int bpm = 80;
+	public float window = 0.2;
+	private MessageRouter MessageRouter;
+
+	// Use this for initialization
+	void Start () {
+		MessageRouter = ServiceFactory.Instance.Resolve<MessageRouter> ();
+		StartCoroutine ("BeatCoroutine");
+	}
+	
+	void OnDisable() {
+		StopCoroutine ("BeatCoroutine");
+	}
+
+	private IEnumerator BeatCoroutine() {
+		while (true) {
+			yield return new WaitForSeconds (60f / bpm * (1 - window));
+			MessageRouter.RaiseMessage (new EnterBeatWindowMessage ());
+			yield return new WaitForSeconds (60f / bpm * window);
+			MessageRouter.RaiseMessage (new ExitBeatWindowMessage ());
+		}
+	}
+}
