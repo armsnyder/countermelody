02f2d2f6f8da1aeda02b45bcd1620bf11fb2c590
Adam Snyder <armsnyder@gmail.com>
2016-02-04 19:06:49 -0600

    Implemented Frictionless event BUS to existing input code, closes #17

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using Frictionless;
+
+public class GameManager : MonoBehaviour {
+
+	void Awake() {
+		// Register MessageRouter (the event BUS) as a singleton so that it can be referenced anywhere
+		ServiceFactory.Instance.RegisterSingleton<MessageRouter> ();
+	}
+}
