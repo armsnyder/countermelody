b55c69abd278682a7ee0cde8259cb974cf0364d0
cegrief <cegrief@gmail.com>
2016-02-05 17:05:40 -0600

    Added Attack Message for Greg's Reference

---
+++ Assets/scripts/interpreter/AttackInterpreter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using Frictionless;
+
+public class AttackUnitMessage {
+	public InputButton Color { get; set; }
+	public int PlayerNumber { get; set; }
+}
+
+public class AttackInterpreter : MonoBehaviour {
+
+	private MessageRouter MessageRouter;
+	private bool IsInBeatWindow;
+
+	// Use this for initialization
+	void Start () {
+		MessageRouter = ServiceFactory.Instance.Resolve<MessageRouter>();
+		MessageRouter.AddHandler<ButtonDownMessage>(OnButtonDown);
+		MessageRouter.AddHandler<EnterBeatWindowMessage>(OnEnterBeatWindow);
+		MessageRouter.AddHandler<ExitBeatWindowMessage>(OnExitBeatWindow);
+	}
+
+	private void OnButtonDown(ButtonDownMessage m) {
+
+	}
+
+	private void OnEnterBeatWindow(EnterBeatWindowMessage m) {
+		Debug.Log("Beat Window Entered");
+		IsInBeatWindow = true;
+	}
+
+	private void OnExitBeatWindow(ExitBeatWindowMessage m) {
+		Debug.Log("Beat Window Exited");
+		IsInBeatWindow = false;
+	}
+	
+}
