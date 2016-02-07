9e20f3b9c55691c47104db9eb739c5fa16d71615
nakfoury <nakfoury@gmail.com>
2016-02-06 21:29:45 -0800

    Update MoveInterpreter.cs
    It's your problem now

--- Assets/scripts/interpreter/MoveInterpreter.cs
+++ Assets/scripts/interpreter/MoveInterpreter.cs
@@ -66,13 +66,22 @@ public class MoveInterpreter : Interpreter {
 	}
 
 	private void OnExitBeatWindow(ExitBeatWindowMessage m) {
-		if (IsAcceptingActions && HeldFrets.ContainsKey(CurrentPlayer) && HeldFrets [CurrentPlayer].Count > 0) {
-			// Send end-of-beat move message
-			MessageRouter.RaiseMessage (new UnitActionMessage () { 
-				ActionType = UnitActionMessageType.MOVE, 
-				PlayerNumber = CurrentPlayer, 
-				Direction = ColorToVector (HeldFrets [CurrentPlayer] [HeldFrets.Count - 1])
-			});
+		switch (HeldFrets [CurrentPlayer] [HeldFrets.Count - 1]) {
+		case InputButton.GREEN:
+		case InputButton.BLUE:
+		case InputButton.YELLOW:
+		case InputButton.RED:
+			if (IsAcceptingActions && HeldFrets.ContainsKey(CurrentPlayer) && HeldFrets [CurrentPlayer].Count > 0) {
+				// Send end-of-beat move message
+				MessageRouter.RaiseMessage (new UnitActionMessage () { 
+					ActionType = UnitActionMessageType.MOVE, 
+					PlayerNumber = CurrentPlayer, 
+					Direction = ColorToVector (HeldFrets [CurrentPlayer] [HeldFrets.Count - 1])
+				});
+			}
+			break;
+		default:
+			break;
 		}
 		IsAcceptingActions = false;
 	}
