9f1697712126e8e7b91a95cf9a45156fa0e9d9d8
Adam Snyder <armsnyder@gmail.com>
2016-02-11 05:25:24 -0600

    Song.cs loads instrument tracks and handles muting/unmuting in battle

---
+++ Assets/scripts/BattleManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using Frictionless;
+
+/// <summary>
+/// Sent when a player hits a note
+/// </summary>
+public class NoteHitMessage {
+	public int PlayerNumber{ get; set; }
+	public int InstrumentID{ get; set; }  // Used by Song.cs to play/mute player's instrument track
+}
+
+/// <summary>
+/// Sent when a player misses a note
+/// </summary>
+public class NoteMissMessage {
+	public int PlayerNumber{ get; set; }
+	public int InstrumentID{ get; set; }  // Used by Song.cs to play/mute player's instrument track
+}
+
+/// <summary>
+/// Sent when a battle begins
+/// </summary>
+public class EnterBattleMessage {
+	/// Right now this is just used by Song.cs to cue unmuting instrument tracks.
+}
+
+/// <summary>
+/// Sent when a battle ends
+/// </summary>
+public class ExitBattleMessage {
+	/// Right now this is just used by Song.cs to cue muting instrument tracks.
+}
+
+public class BattleManager : MonoBehaviour {
+	
+}
