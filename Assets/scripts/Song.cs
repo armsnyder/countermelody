6e4bbc5361e9df9444cb8162f5197d8954e96839
cegrief <cegrief@gmail.com>
2016-02-13 14:23:25 -0600

    Changed Note to NoteObject,fixed some scene stuff

--- Assets/scripts/Song.cs
+++ Assets/scripts/Song.cs
@@ -41,15 +41,22 @@ public class Note {
 	public int difficulty { get; set; } // Easy = 0, medium = 1, hard = 2
 	public int duration { get; set; } // Number of position units the note lasts (120 = 16th note)
 	public int velocity { get; set; } // Extraneous MIDI information we might use late on, but for now is always 127
-	public int fretNumber {get; set; } // Number of fret (0 = green, 4 = orange)
-	public InputButton fretColor { get { return fretNumber == 0 ? InputButton.GREEN : fretNumber == 1 ? 
-			InputButton.RED : fretNumber == 2 ? InputButton.YELLOW : fretNumber == 3 ? InputButton.BLUE : 
-			InputButton.ORANGE; } } // Color of fret as InputButton
-	public bool isHOPO {get {return velocity < 127;}} // If true, strum not required to hit note
+	public int fretNumber { get; set; } // Number of fret (0 = green, 4 = orange)
+	public InputButton fretColor
+	{
+		get
+		{
+			return fretNumber == 0 ? InputButton.GREEN : fretNumber == 1 ?
+				InputButton.RED : fretNumber == 2 ? InputButton.YELLOW : fretNumber == 3 ?
+				InputButton.BLUE : InputButton.ORANGE;
+		}
+	} // Color of fret as InputButton
+	public bool isHOPO { get { return velocity < 127; } } // If true, strum not required to hit note
 	public float getPositionTime(float bpm) {
 		return position / 360f / bpm * 60f;
 	}
 }
+
 public class Song : MonoBehaviour {
 
 	public float bpm = 80f;
