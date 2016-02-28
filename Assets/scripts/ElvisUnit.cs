71086b0ae92b97951e9c917a1f3c1ad24d6069e1
cegrief <cegrief@gmail.com>
2016-02-28 16:40:55 -0600

    fixed some bugs with the display

--- Assets/scripts/ElvisUnit.cs
+++ Assets/scripts/ElvisUnit.cs
@@ -2,17 +2,5 @@
 using System.Collections;
 
 public class ElvisUnit : MelodyUnit {
-
-	// Use this for initialization
-	void Start () {
-		HitPoints = 100;
-		AttackRange = 3;
-		AttackFactor = 100;
-		DefenceFactor = 50;
-		MovementPoints = 4;
-		MovementSpeed = 8;
-		ActionPoints = 1;
-		hopHeight = 1;
-	}
 	
 }
--- Assets/scripts/JacksonUnit.cs
+++ Assets/scripts/JacksonUnit.cs
@@ -2,17 +2,5 @@
 using System.Collections;
 
 public class JacksonUnit : MelodyUnit {
-
-	// Use this for initialization
-	void Start () {
-		HitPoints = 100;
-		AttackRange = 3;
-		AttackFactor = 100;
-		DefenceFactor = 50;
-		MovementPoints = 4;
-		MovementSpeed = 8;
-		ActionPoints = 1;	
-		hopHeight = 1;
-	}
 	
 }
