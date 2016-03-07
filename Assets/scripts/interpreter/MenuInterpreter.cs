38e0c1d1abdf72999436c5030933bd072659dc14
cegrief <cegrief@gmail.com>
2016-03-07 17:41:31 -0600

    updated a ton of stuff so scene changes worknow

---
+++ Assets/scripts/interpreter/MenuInterpreter.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigateMenuMessage {
+	public NavigationType NavType;
+	public int PlayerNumber;
+}
+
+public enum NavigationType {
+	SCROLL_UP,
+	SCROLL_DOWN,
+	SELECT,
+	BACK,
+	EXIT
+}
+
+public class MenuInterpreter : Interpreter {
+
+	protected override void OnButtonDown(ButtonDownMessage m) {
+		switch (m.Button) {
+			case InputButton.STRUM_UP:
+			case InputButton.UP:
+				MessageRouter.RaiseMessage(new NavigateMenuMessage() {
+					PlayerNumber = m.PlayerNumber,
+					NavType = NavigationType.SCROLL_UP
+				});
+				break;
+			case InputButton.STRUM_DOWN:
+			case InputButton.DOWN:
+				MessageRouter.RaiseMessage(new NavigateMenuMessage() {
+					PlayerNumber = m.PlayerNumber,
+					NavType = NavigationType.SCROLL_DOWN
+				});
+				break;
+			case InputButton.GREEN:
+				MessageRouter.RaiseMessage(new NavigateMenuMessage() {
+					PlayerNumber = m.PlayerNumber,
+					NavType = NavigationType.SELECT
+				});
+				break;
+			case InputButton.RED:
+				MessageRouter.RaiseMessage(new NavigateMenuMessage() {
+					PlayerNumber = m.PlayerNumber,
+					NavType = NavigationType.BACK
+				});
+				break;
+			case InputButton.PLUS:
+				MessageRouter.RaiseMessage(new NavigateMenuMessage() {
+					PlayerNumber = m.PlayerNumber,
+					NavType = NavigationType.EXIT
+				});
+				break;
+			default:
+				break;
+		}
+	}
+}
---
+++ Assets/scripts/menu/MainMenuManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using Frictionless;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+	MessageRouter MessageRouter;
+	int CurrentSelectionIndex = 0;
+	[SerializeField]
+	private List<Text> MenuOptions;
+
+	void Awake () {
+		ServiceFactory.Instance.RegisterSingleton<MessageRouter>();
+		MessageRouter = ServiceFactory.Instance.Resolve<MessageRouter>();
+		MessageRouter.AddHandler<NavigateMenuMessage>(OnNavigateMenu);
+		MessageRouter.AddHandler<SceneChangeMessage>(OnSceneChange);
+	}
+
+	void Start() {
+		MenuOptions[CurrentSelectionIndex].GetComponentInChildren<Image>().enabled = true;
+	}
+
+	void OnNavigateMenu(NavigateMenuMessage m) {
+		switch (m.NavType) {
+			case NavigationType.SCROLL_UP:
+				Scroll(-1);
+				break;
+			case NavigationType.SCROLL_DOWN:
+				Scroll(1);
+				break;
+			case NavigationType.SELECT:
+				MakeCurrentSelection();
+				break;
+			default:
+				break;
+		}
+	}
+
+	void Scroll(int dir) {
+		MenuOptions[CurrentSelectionIndex].GetComponentInChildren<Image>().enabled = false;
+		CurrentSelectionIndex = (CurrentSelectionIndex + dir + MenuOptions.Count) % MenuOptions.Count;
+		MenuOptions[CurrentSelectionIndex].GetComponentInChildren<Image>().enabled = true;
+	}
+
+	void MakeCurrentSelection() {
+		switch (MenuOptions[CurrentSelectionIndex].GetComponent<MainMenuItem>().ItemType) {
+			case MenuItemType.LoadScene:
+				MessageRouter.RaiseMessage(new SceneChangeMessage() {
+					nextScene = MenuOptions[CurrentSelectionIndex].GetComponent<MainMenuItem>().SceneName
+				});
+				break;
+			case MenuItemType.ExitGame:
+				Application.Quit();
+				break;
+			default:
+				break;
+		}
+	}
+
+	void OnSceneChange(SceneChangeMessage m) {
+		StartCoroutine(RemoveHandlers(m.nextScene));
+	}
+
+	IEnumerator RemoveHandlers(string nextScene) {
+		yield return new WaitForEndOfFrame();
+		MessageRouter.RemoveHandler<NavigateMenuMessage>(OnNavigateMenu);
+		MessageRouter.RemoveHandler<SceneChangeMessage>(OnSceneChange);
+		SceneManager.LoadScene(nextScene);
+	}
+
+}
