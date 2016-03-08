c21eaa0752750120206ce872ed6852eb135e4d58
GregoryElliott <gregoryelliott34@yahoo.com>
2016-03-07 18:03:28 -0600

    remove debug

--- Assets/scripts/util/GameObjectUtil.cs
+++ Assets/scripts/util/GameObjectUtil.cs
@@ -13,53 +13,24 @@ public class GameObjectUtil : MonoBehaviour {
 	private static Dictionary<RecycleGameObject, ObjectPool> pools = new Dictionary<RecycleGameObject, ObjectPool> ();
 
 	private MessageRouter messageRouter;
-	//private static bool ignore;
 
 	void Start() {
-		//ignore = false;
 		pools = new Dictionary<RecycleGameObject, ObjectPool> ();
 		messageRouter = ServiceFactory.Instance.Resolve<MessageRouter> ();
 		messageRouter.AddHandler<SceneChangeMessage> (OnSceneChange);
 	}
 
 	void OnSceneChange(SceneChangeMessage m) {
-		//ignore = true;
-		//StartCoroutine(RemoveHandlers());
 		pools = new Dictionary<RecycleGameObject, ObjectPool> ();
-		/*var recycledScript = prefab.GetComponent<RecycleGameObject> ();
-
-		var pool = GetObjectPool (recycledScript);
-		instance = pool.NextObject (pos).gameObject;
-		while (instance != null) {
-			Destroy(instance);
-			instance = pool.NextObject (pos).gameObject;
-		}*/
 	}
 
-	/*IEnumerator RemoveHandlers() {
-		yield return new WaitForEndOfFrame();		
-		messageRouter.RemoveHandler<SceneChangeMessage> (OnSceneChange);
-	}*/
-
 	public static GameObject Instantiate(GameObject prefab, Vector3 pos){
-		/*if (ignore) {
-			return null;
-		}*/
 		GameObject instance = null;
 
 		var recycledScript = prefab.GetComponent<RecycleGameObject> ();
-		Debug.Log(recycledScript);
 		if (recycledScript != null) {
 			var pool = GetObjectPool (recycledScript);
-			Debug.Log(pool);
-			/*if (pool == null) {
-				GameObjectUtil.Destroy(recycledScript.gameObject);
-				instance = GameObject.Instantiate (prefab);
-				instance.transform.position = pos;
-			}*/
-			//else {
-			instance = pool.NextObject (pos).gameObject; 
-			//}
+			instance = pool.NextObject (pos).gameObject; 		
 		} else {
 
 			instance = GameObject.Instantiate (prefab);
@@ -85,13 +56,10 @@ public class GameObjectUtil : MonoBehaviour {
 
 	private static ObjectPool GetObjectPool(RecycleGameObject reference){
 		ObjectPool pool = null;
-		Debug.Log(pools);
 
 		if (pools.ContainsKey (reference)) {
-			Debug.Log("here1");
 			pool = pools [reference];
 		} else {
-			Debug.Log("here2");
 			var poolContainer = new GameObject(reference.gameObject.name + "ObjectPool");
 			pool = poolContainer.AddComponent<ObjectPool>();
 			pool.prefab = reference;
